22def3f3b33260501b1ebc3ceaa904db1158e880
贾九月 <jiajiuyue@jiajiuyuedeiMac.local>
2018-11-15 17:17:22 +0800

    add command

---
+++ command/upload.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/expect -f
+set password c4IbHcLWFAat
+spawn scp -r /Users/jiajiuyue/.jenkins/workspace/Myblog/dist/ root@47.96.104.26:~/Sept/Test/Myblog
+set timeout 300
+expect "root@47.96.104.26's password"
+set timeout 300
+send "$password\r"
+set timeout 300
+send "exit\r"
+expect eof
