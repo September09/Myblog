9e68368c30082a50577c1932c40158a435d4e0e9
September <15010639928@163.com>
2018-11-24 08:42:30 +0800

    change name

--- command/upload.sh
+++ command/upload.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect -f
 set password c4IbHcLWFAat
-spawn scp -r /Users/jiajiuyue/.jenkins/workspace/Myblog/dist/ root@47.96.104.26:~/Sept/Test/Myblog
+spawn scp -r /Users/september/.jenkins/workspace/Myblog/dist/ root@47.96.104.26:~/Sept/Test/Myblog
 set timeout 300
 expect "root@47.96.104.26's password"
 set timeout 300
